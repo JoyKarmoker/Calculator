02efeb921b300899437a8a6066caefb36fa07ab7
Rakibul Islam <rakibul.rucse26@gmail.com>
2019-04-17 10:44:39 +0600

    Multiplication

--- Calculator.java
+++ Calculator.java
@@ -5,18 +5,3 @@
  	}
 	
 }
-
-public class CalMultiplication{
-	int result;
-	CalMultiplication(int a, int b){
-
-		result = a * b;
-
-	}
-
-	void printResult(){
-
-		System.out.pirntln("The result of multiplication is : " + result);
-	
-	}
-}
